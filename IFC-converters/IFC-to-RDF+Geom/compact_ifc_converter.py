86d0c594e7527f88f2b9bc318357f7ecaf3e7957
cramonell <cramonell@hotmail.com>
2025-07-08 15:15:58 +0200

    workflow test #5

--- IFC-converters/IFC-to-RDF+Geom/compact_ifc_converter.py
+++ IFC-converters/IFC-to-RDF+Geom/compact_ifc_converter.py
@@ -3,7 +3,7 @@ Compact IFC Converter - Complete IFC to RDF + GLB conversion
 Author: Assistant
 Description: Self-contained converter for IFC files to RDF metadata and GLB geometry
 """
-#workflow test comment 
+#workflow test comment 1
 
 import ifcopenshell
 import ifcopenshell.geom
